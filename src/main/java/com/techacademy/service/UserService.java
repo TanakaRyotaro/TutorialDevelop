b88d5c05baa98bf4d3de41abb66ec061ee8bb336
Tanaka Ryotaro <c-rytanaka@yahoo-corp.jp>
2023-01-16 18:00:24 +0900

    Userの登録項目を追加

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,9 @@
 package com.techacademy.service;
 
 import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.techacademy.entity.User;
@@ -15,5 +18,10 @@ public class UserService {
     public List<User> getUserList() {
         //リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
+    }
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 }
