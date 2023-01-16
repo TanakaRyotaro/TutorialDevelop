13c420363fa945da20015cf7f1aa61b07a4070cd
Tanaka Ryotaro <c-rytanaka@yahoo-corp.jp>
2023-01-16 14:50:58 +0900

    User情報管理システムの追加

--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -14,24 +14,31 @@ import lombok.Data;
 @Entity
 @Table(name = "user")
 public class User {
+
     /** 性別用の列挙型 */
     public static enum Gender {
         男性, 女性
     }
+
     /** 主キー。自動生成 */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     /** 名前。20桁。null不許可 */
-    @Column(length=20, nullable=false )
+    @Column(length = 20, nullable = false)
     private String name;
-    /** 性別。２桁。列挙型(文字列) */
+
+    /** 性別。2桁。列挙型（文字列） */
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
     private Gender gender;
+
     /** 年齢 */
     private Integer age;
+
     /** メールアドレス。50桁。null許可 */
     @Column(length = 50)
     private String email;
+
 }
