657e2e92e08b818632ff4161c6242289048ed298
Tanaka Ryotaro <c-rytanaka@yahoo-corp.jp>
2023-01-18 15:43:01 +0900

    Lesson17 Chapter11 Commit

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.techacademy.controller;
 
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
 
@@ -32,7 +36,11 @@ public class UserController {
     }
     /** User登録処理 */
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user, BindingResult res,Model model) {
+        if(res.hasErrors()) {
+            //エラーあり
+            return getRegister(user);
+        }
         // User登録
         service.saveUser(user);
         //一覧画面にリダイレクト
@@ -52,6 +60,14 @@ public class UserController {
         // User登録
         service.saveUser(user);
         //一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    /** User削除処理 */
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
+        // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
 }
